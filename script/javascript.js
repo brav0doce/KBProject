b665d35a94b92886db5a9ed295c49f8fd6a3fdc0
brav0doce <95630305+brav0doce@users.noreply.github.com>
2022-03-12 18:53:37 +0100

    Add files via upload

---
+++ script/javascript.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+					if (window.location.href.indexOf("65%") > -1) {
+					  document.getElementById("60%").checked = true;
+					}
+					else if (window.location.href.indexOf("80%") > -1) {
+					  document.getElementById("80%").checked = true;
+					}
+					else if (window.location.href.indexOf("100%") > -1) {
+					  document.getElementById("100%").checked = true;
+					}
+					else if (window.location.href.indexOf("110%") > -1) {
+					  document.getElementById("110%").checked = true;
+					}
+				  });
